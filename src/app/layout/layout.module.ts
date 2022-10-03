abc4237b79293f5c4d605467a4f35babd4b4e39b
Fernando Asmuzi <rupa454@RENTAS.com>
2022-10-03 10:38:01 -0300

    creación del proyecto y primeros componentes

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    ProfileComponent,
+    DashboardComponent,
+    SkillsComponent,
+    EducationComponent,
+    WorkExperienceComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ]
+})
+export class LayoutModule { }
